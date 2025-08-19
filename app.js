8aecdbf043182f00dc5b00860020128aa5158359
DraEleonora <eleonorabc@gmail.com>
2025-08-19 12:02:24 -0300

    Implementa uma função para adicionar amigos

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 let amigos = []
+
+// Função para adicionar amigos
+function adicionarAmigo() {
+    let input = document.getElementById("amigo");
+    let nome = input.value.trim();
+
+    if (nome === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+
+    amigos.push(nome);
+    input.value = ""; // limpa o campo
+    atualizarLista();
+}
+
